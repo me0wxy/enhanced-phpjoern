aa4cc59d3c659f7c3720c19fd23c1cbc1b235d36
wzw97 <bantttian@gmail.com>
2021-06-18 13:33:16 +0800

    testcases

---
+++ TestCases/30vs70Case/CHG/Trait/october-2.0.10/ForwardsCalls.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Illuminate\Support\Traits;
+
+trait ForwardsCalls
+{
+    /**
+     * Forward a method call to the given object.
+     *
+     * @param  mixed  $object
+     * @param  string  $method
+     * @param  array  $parameters
+     * @return mixed
+     *
+     * @throws \BadMethodCallException
+     */
+    protected function forwardCallTo($object, $method, $parameters)
+    {
+        // ...
+
+    }
+
+}
+
